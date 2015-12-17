c17aba0a17cfd14b551385eea71191bee6eb8130
Antony S <alonestalonne@gmail.com>
2015-12-17 15:07:32 +0200

    add mocha

--- app/main.js
+++ app/main.js
@@ -1,8 +1,8 @@
 var express = require('express')
-  , app = express()
+    , app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello, Express!')
+    res.send('Hello, Express!')
 })
 
 module.exports = app;
